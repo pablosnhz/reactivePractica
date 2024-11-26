ad54d5a3d72d09c556ea94a211dd94ec9167d3dc
PabloSnhz <pablosanhuezaaa@gmail.com>
2024-11-25 21:35:39 -0300

    subject suscription examples

--- src/app/signals/rxjs/rxjs/rxjs.component.ts
+++ src/app/signals/rxjs/rxjs/rxjs.component.ts
@@ -11,14 +11,18 @@ export class RxjsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    // subject multiples suscriptores
     const subject = new Subject<number>();
 
+    // nos suscrimos al subject
     subject.subscribe({
       next: (n) => console.log(`Observer A: ${n}`)
     })
     subject.subscribe({
       next: (n) => console.log(`Observer B: ${n}`)
     })
+
+    // y lo mandamos junto con el next
     subject.next(1);
     subject.next(2);
   }
