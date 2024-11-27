edf801e918832fcfb2d840cff9cd954bbafae367
PabloSnhz <pablosanhuezaaa@gmail.com>
2024-11-26 21:26:47 -0300

    use of fromEvent, interval, timer, filter and map

--- src/app/signals/rxjs/rxjs/rxjs.component.ts
+++ src/app/signals/rxjs/rxjs/rxjs.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Subject } from 'rxjs';
+import { filter, fromEvent, interval, map, of, Subject, timer } from 'rxjs';
 
 @Component({
   selector: 'app-rxjs',
@@ -11,20 +11,47 @@ export class RxjsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-    // subject multiples suscriptores
-    const subject = new Subject<number>();
 
-    // nos suscrimos al subject
-    subject.subscribe({
-      next: (n) => console.log(`Observer A: ${n}`)
-    })
-    subject.subscribe({
-      next: (n) => console.log(`Observer B: ${n}`)
-    })
+    // ! MAP y FILTER haciendo uso de pipe y of
+    const nums = of(1, 2, 3, 4, 5).pipe(
+      filter((n: number) => {
+        // obteniendo pares
+        return n % 2 === 0;
+      }),
+      map((n: number) => {
+        return n * n;
+      })
+    );
 
-    // y lo mandamos junto con el next
-    subject.next(1);
-    subject.next(2);
+    nums.subscribe((n: number) => {
+      console.log(n);
+    });
+
+
+    // ! INTERVAL o Timer para emitir un valor cada cierto tiempo
+    // // interval o timer
+    // const contador = timer(1000);
+
+    // contador.subscribe((n) => {
+    //   console.log('Trigger');
+
+    // })
+
+    // ! SUBJECT
+    // // subject multiples suscriptores
+    // const subject = new Subject<number>();
+
+    // // nos suscrimos al subject
+    // subject.subscribe({
+    //   next: (n) => console.log(`Observer A: ${n}`)
+    // })
+    // subject.subscribe({
+    //   next: (n) => console.log(`Observer B: ${n}`)
+    // })
+
+    // // y lo mandamos junto con el next
+    // subject.next(1);
+    // subject.next(2);
   }
 
 }
